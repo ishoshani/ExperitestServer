fa3fa5f6f9ee005306e6e65525039f382f2f51d0
isho <idoshoshani@gmail.com>
2017-10-21 22:41:09 -0700

    deployment?

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template
 from datetime import datetime
 import os
 import time
-import urllib3
+import urllib
 import json
 app = Flask(__name__)
 
@@ -14,6 +14,6 @@ def hello_time():
     os.environ["TZ"]=zone
     time.tzset()
     nowtime = datetime.now().time();
-    f = urllib3.urlopen("http://api.fixer.io/latest?base=USD&symbols=ILS")
+    f = urllib.urlopen("http://api.fixer.io/latest?base=USD&symbols=ILS")
     js = json.loads(f.read())
     return render_template("stats.html",nowtime=nowtime.strftime("%H:%M:%S %Z"),nowExchange = js["rates"]["ILS"])
